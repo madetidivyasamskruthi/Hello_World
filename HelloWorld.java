d238f4f2b85bb43707c834588288bacd9b8658b8
madetidivyasamskruthi <madetidivyasamskruthi@gmail.com>
2026-04-13 20:20:25 +0530

    UC6: Enhanced loop with substring to remove trailing comma

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,16 @@ public class HelloWorld {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            System.out.print("Hello ");
+            String result = "";
+
             for (String name : args) {
-                System.out.print(name + " ");
+                result += name + ", ";
             }
-            System.out.println();
+
+            // remove last ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello " + result);
         }
     }
 }
