63cb25e295c445c8590f4ce0028c08f39b0e5154
madetidivyasamskruthi <madetidivyasamskruthi@gmail.com>
2026-04-13 20:37:26 +0530

    Fix UC3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello");
-        }
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello " + name);
     }
 }
